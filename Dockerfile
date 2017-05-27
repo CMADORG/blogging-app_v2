41737b54038f0122527314274fc7fa2fa110f97c
Sankar P S (sansaras) <sansaras@cisco.com>
2017-05-27 19:37:25 +0530

    change the app path

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM tomcat:latest
-ADD target/*.war /usr/local/tomcat/webapps/CMAD1.war
+ADD target/CMAD1*.war /usr/local/tomcat/webapps/CMAD1.war
 ADD setenv.sh /usr/local/tomcat/bin/setenv.sh
--- src/main/java/com/mycmad/rest/UserRestServices.java
+++ src/main/java/com/mycmad/rest/UserRestServices.java
@@ -25,7 +25,7 @@ public class UserRestServices{
 	@POST
 	@Path("/user")
 	@Consumes(MediaType.APPLICATION_JSON)
-	public  Response addUser(User user)
+	public  Response addUsers(User user)
 	{
 		//Request
 		ums.addUser(user);		
