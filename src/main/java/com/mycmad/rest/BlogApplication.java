466f41d384899110a5ba46d34b1f3aa933fc2d1d
Sankar P S (sansaras) <sansaras@cisco.com>
2017-05-27 12:07:34 +0530

    change the app path

--- src/main/java/com/mycmad/rest/BlogApplication.java
+++ src/main/java/com/mycmad/rest/BlogApplication.java
@@ -4,7 +4,7 @@ import javax.ws.rs.ApplicationPath;
 
 import org.glassfish.jersey.server.ResourceConfig;
 
-@ApplicationPath("")
+@ApplicationPath("rest")
 public class BlogApplication extends ResourceConfig {
 	public BlogApplication() {
 		packages("CMAD.rest");
